721e551ddf3dc1ac97bcbc593b1f615d20c2f2a7
askar-ya <askaryaparov@yandex.ru>
2025-01-11 23:53:46 +0300

    d

--- app/urls.py
+++ app/urls.py
@@ -3,8 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.main_page, name='main_page'),
-    path('search/', views.search_q, name='about'),
+    path('', views.search_q, name='Reels Scaner'),
     path('add_authors/', views.add_authors),
     path('add_authors/add/', views.add_authors_post),
 ]

fe88cde7ff48ec98b71627d3c0ac8ba42d6c9c3f
askar-ya <askaryaparov@yandex.ru>
2025-01-22 05:27:57 +0300

    fix

--- config/settings.py
+++ config/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.yandex.ru'
-EMAIL_PORT = 587
+EMAIL_PORT = 465
 EMAIL_USE_TLS = False
 EMAIL_USE_SSL = True
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
